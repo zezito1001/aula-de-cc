7c502900d919948620a43e7a3e6f1396f2532f91
zezito1001 <brunofelix.silva15@gmail.com>
2026-08-25 21:10:17 -0300

    Create Atividade calculo de media.py

---
+++ Atividade calculo de media.py
@@ -0,0 +1,10 @@
+n1=float(input("Nota 1:"))
+n2=float(input("Nota 2:"))
+n3=float(input("Nota 3:"))
+
+Nota_Final= (n1+n2+n3)/3
+
+Aprovado= Nota_Final >=7.0
+
+print("Nota_Fina", Nota_Final)
+print("Aprovado", Aprovado)
