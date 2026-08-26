3a1fc6475af4a43db0d7f9fd46937fd2433bdb2c
zezito1001 <brunofelix.silva15@gmail.com>
2026-08-25 21:23:19 -0300

    Create calculo de preco com desconto.py

---
+++ calculo de preco com desconto.py
@@ -0,0 +1,8 @@
+preço=float(input("Digite o Valor do Produto: "))
+Desconto=float(input("Digite o Valor do Desconto"))
+
+Desconto= preço*Desconto/100
+
+Preço_Final=preço - Desconto
+
+print(Preço_Final)
