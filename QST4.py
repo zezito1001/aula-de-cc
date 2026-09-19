0b77a9ab76b4a3d3d282d444053dad4e4f152bdf
zezito1001 <brunofelix.silva15@gmail.com>
2026-09-19 16:44:31 -0300

    Create QST4.py

---
+++ QST4.py
@@ -0,0 +1,11 @@
+Gerente = 8500
+Analista = 5000
+Suporte = 3000
+
+novo_salario_gerente =  Gerente*(1+12/100)
+novo_salario_analista = Analista*(1+12/100)
+novo_salario_suporte = Suporte*(1+15/100)
+
+print(f"O novo salario do gerente é {novo_salario_gerente:.2f}")
+print(F"O novo salario do analista é {novo_salario_analista:.2f}")
+print(f"O novo salario do suporte é {novo_salario_suporte:.2f}")
